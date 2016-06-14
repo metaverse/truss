2c21186c24fe41726faa0291a1a905ad44d24c4c
Adam Ryman <adamr@tune.com>
2016-06-14 14:22:39 -0700

    Remove logging from entity helper

--- protoc-gen-gokit-base/to-generate/entityhelper/entity_helper.go
+++ protoc-gen-gokit-base/to-generate/entityhelper/entity_helper.go
@@ -1,27 +1,11 @@
 package entityhelper
 
 import (
-	stdlog "log"
-	"os"
-
-	"github.com/go-kit/kit/log"
-	"github.com/go-kit/kit/log/levels"
 	"github.com/TuneLab/gob/protoc-gen-gokit-base/to-generate/mv-shared/mysql"
 )
 
 type EntityHelper struct {
 	*mysql.Client
-}
-
-var (
-	logger levels.Levels
-)
-
-func init() {
-	klogger := log.NewJSONLogger(os.Stdout)
-	logger = levels.New(klogger)
-	stdlog.SetFlags(0)                              // flags are handled by Go kit's logger
-	stdlog.SetOutput(log.NewStdlibAdapter(klogger)) // redirect anything using stdlib log to us
 }
 
 func (c *EntityHelper) GetMysqlClient() (*mysql.Client, error) {
