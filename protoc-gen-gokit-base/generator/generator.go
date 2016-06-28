a1349f4acf4d0dde9f62d1277f0729cd4c0ce24c
Adam Ryman <adamr@tune.com>
2016-06-28 11:47:53 -0700

    Change import to use templateExecute struct for absRelPath

--- protoc-gen-gokit-base/generator/generator.go
+++ protoc-gen-gokit-base/generator/generator.go
@@ -30,6 +30,18 @@ type generator struct {
 	templateFile      func(string) ([]byte, error)
 }
 
+type templateExecutor struct {
+}
+
+func (t templateExecutor) AbsoluteRelativeImportPath() string {
+	wd, _ := os.Getwd()
+	goPath := os.Getenv("GOPATH")
+	importPath := strings.TrimPrefix(wd, goPath+"/src/")
+	importPath = importPath + "/generate/"
+
+	return importPath
+}
+
 var (
 	response = string("")
 )
@@ -45,40 +57,40 @@ func New(reg *descriptor.Registry) *generator {
 	var imports []descriptor.GoPackage
 	// Loop through base golang imports and add them to the generator
 	// If there are conflicts, use Alias function of registry
-	for _, pkgpath := range []string{
-		"fmt",
-		"log",
-		"math/rand",
-		"net",
-		"os",
-		"os/signal",
-		"strconv",
-		"syscall",
-		"time",
+	//for _, pkgpath := range []string{
+	//"fmt",
+	//"log",
+	//"math/rand",
+	//"net",
+	//"os",
+	//"os/signal",
+	//"strconv",
+	//"syscall",
+	//"time",
 
-		"github.com/go-kit/kit/log",
-		"github.com/go-kit/kit/log/levels",
-		"github.com/TuneLab/gob/protoc-gen-gokit-base/generate/controller",
-		"github.com/TuneLab/gob/protoc-gen-gokit-base/generate/pb",
-		"github.com/TuneLab/gob/protoc-gen-gokit-base/generate/server",
-		"google.golang.org/grpc",
-	} {
-		pkg := descriptor.GoPackage{
-			Path: pkgpath,
-			Name: path.Base(pkgpath),
-		}
-		if err := reg.ReserveGoPackageAlias(pkg.Name, pkg.Path); err != nil {
-			for i := 0; ; i++ {
-				alias := fmt.Sprintf("%s_%d", pkg.Name, i)
-				if err := reg.ReserveGoPackageAlias(alias, pkg.Path); err != nil {
-					continue
-				}
-				pkg.Alias = alias
-				break
-			}
-		}
-		imports = append(imports, pkg)
-	}
+	//"github.com/go-kit/kit/log",
+	//"github.com/go-kit/kit/log/levels",
+	//"github.com/TuneLab/gob/protoc-gen-gokit-base/generate/controller",
+	//"github.com/TuneLab/gob/protoc-gen-gokit-base/generate/pb",
+	//"github.com/TuneLab/gob/protoc-gen-gokit-base/generate/server",
+	//"google.golang.org/grpc",
+	//} {
+	//pkg := descriptor.GoPackage{
+	//Path: pkgpath,
+	//Name: path.Base(pkgpath),
+	//}
+	//if err := reg.ReserveGoPackageAlias(pkg.Name, pkg.Path); err != nil {
+	//for i := 0; ; i++ {
+	//alias := fmt.Sprintf("%s_%d", pkg.Name, i)
+	//if err := reg.ReserveGoPackageAlias(alias, pkg.Path); err != nil {
+	//continue
+	//}
+	//pkg.Alias = alias
+	//break
+	//}
+	//}
+	//imports = append(imports, pkg)
+	//}
 	return &generator{
 		reg:               reg,
 		baseImports:       imports,
@@ -90,7 +102,7 @@ func New(reg *descriptor.Registry) *generator {
 func (g *generator) GenerateResponseFiles(targets []*descriptor.File) ([]*plugin.CodeGeneratorResponse_File, error) {
 	var codeGenFiles []*plugin.CodeGeneratorResponse_File
 	for _, file := range g.templateFileNames() {
-		//logf("%v\n", paths)
+		logf("%v\n", file)
 		curResponseFile := plugin.CodeGeneratorResponse_File{}
 
 		// Remove "template_files/" so that generated files do not include that directory
@@ -104,6 +116,7 @@ func (g *generator) GenerateResponseFiles(targets []*descriptor.File) ([]*plugin
 
 		// Currently only templating main.go
 		if path.Base(file) == "main.go" {
+			logf("%v\n", "entering main.go")
 			stringFile, _ = g.MyGenerate(targets, file, bytesOfFile)
 		}
 		curResponseFile.Content = &stringFile
@@ -119,11 +132,12 @@ func (g *generator) MyGenerate(targets []*descriptor.File, templateName string, 
 	headerTemplate = template.Must(template.New(templateName).Parse(templateString))
 	//var files []*plugin.CodeGeneratorResponse_File
 
-	//logf("%v\n", templateString)
+	logf("%v\n", len(targets))
+
 	for _, file := range targets {
 		glog.V(1).Infof("Processing %s", file.GetName())
 		code, err := g.generate(file)
-		//logf("%v\n", code)
+		logf("%v\n", code)
 		if err == errNoTargetService {
 			glog.V(1).Infof("%s: %v", file.GetName(), err)
 			continue
@@ -171,8 +185,9 @@ func applyTemplate(p param) (string, error) {
 	w := bytes.NewBuffer(nil)
 	//logf("%v\n", p.GetSourceCodeInfo())
 	//logf("%v\n", p)
-	if err := headerTemplate.Execute(w, p); err != nil {
-		return "", err
+	_ = p
+	if err := headerTemplate.Execute(w, templateExecutor{}); err != nil {
+		return "FAIL", err
 	}
 	//logf("%v\n", w.String())
 	//var methodSeen bool
