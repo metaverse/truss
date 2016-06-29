fe8dec6333a8c226fafd32db450c150bbf7da7ef
Adam Ryman <adamr@tune.com>
2016-06-29 14:26:58 -0700

    Add descriptor service to templateEx, grpc_binding.go now generates

--- protoc-gen-gokit-base/generator/generator.go
+++ protoc-gen-gokit-base/generator/generator.go
@@ -22,6 +22,7 @@ type generator struct {
 }
 
 type templateExecutor struct {
+	Service *descriptor.Service
 }
 
 // Get working directory, trim off GOPATH, add generate.
@@ -37,12 +38,21 @@ func (t templateExecutor) AbsoluteRelativeImportPath() string {
 
 // New returns a new generator which generates grpc gateway files.
 func New(reg *descriptor.Registry, files []*descriptor.File) *generator {
+	var service *descriptor.Service
+	for _, file := range files {
+		util.Logf("File: %v\n", file.GetName())
+		if len(file.Services) > 0 {
+			service = file.Services[0]
+		}
+	}
 	return &generator{
 		reg:               reg,
 		files:             files,
 		templateFileNames: templateFileAssets.AssetNames,
 		templateFile:      templateFileAssets.Asset,
-		templateExec:      templateExecutor{},
+		templateExec: templateExecutor{
+			Service: service,
+		},
 	}
 }
 
@@ -51,21 +61,20 @@ func (g *generator) GenerateResponseFiles(targets []*descriptor.File) ([]*plugin
 
 	g.printAllServices()
 
-	for _, file := range g.templateFileNames() {
-		util.Logf("%v\n", file)
+	for _, templateFile := range g.templateFileNames() {
 		curResponseFile := plugin.CodeGeneratorResponse_File{}
 
 		// Remove "template_files/" so that generated files do not include that directory
-		d := strings.TrimPrefix(file, "template_files/")
+		d := strings.TrimPrefix(templateFile, "template_files/")
 		curResponseFile.Name = &d
 
 		// Get the bytes from the file we are working on
 		// then turn it into a string to build a template out of it
-		bytesOfFile, _ := g.templateFile(file)
+		bytesOfFile, _ := g.templateFile(templateFile)
 		stringFile := string(bytesOfFile)
 
 		// Currently only templating main.go
-		stringFile, _ = generate(targets, file, bytesOfFile)
+		stringFile, _ = g.generate(templateFile, bytesOfFile)
 		curResponseFile.Content = &stringFile
 
 		codeGenFiles = append(codeGenFiles, &curResponseFile)
@@ -74,7 +83,7 @@ func (g *generator) GenerateResponseFiles(targets []*descriptor.File) ([]*plugin
 	return codeGenFiles, nil
 }
 
-func (g *generator) printAllServices() {
+func (g *generator) test() {
 	for _, file := range g.files {
 		util.Logf("File: %v\n", file.GetName())
 		for _, service := range file.Services {
@@ -89,18 +98,39 @@ func (g *generator) printAllServices() {
 	}
 }
 
-func generate(targets []*descriptor.File, templateName string, templateBytes []byte) (string, error) {
+func (g *generator) printAllServices() {
+	for _, file := range g.files {
+		util.Logf("File: %v\n", file.GetName())
+		if len(file.Services) > 0 {
+			service := file.Services[0]
+			util.Logf("\tService: %v\n", service.GetName())
+			for _, method := range service.Methods {
+				util.Logf("\t\tMethod: %v\n", method.GetName())
+				util.Logf("\t\t\t Request: %v\n", method.RequestType.GetName())
+				util.Logf("\t\t\t Response: %v\n", method.ResponseType.GetName())
+				util.Logf("\t\t\t\tOptions: %v\n", method.Options.String())
+			}
+		}
+	}
+}
+
+func (g *generator) generate(templateName string, templateBytes []byte) (string, error) {
 
 	templateString := string(templateBytes)
 
 	codeTemplate := template.Must(template.New(templateName).Parse(templateString))
 
 	w := bytes.NewBuffer(nil)
-	err := codeTemplate.Execute(w, templateExecutor{})
+	err := codeTemplate.Execute(w, g.templateExec)
 	if err != nil {
 		return "", err
 	}
+
 	code := w.String()
+	if strings.Contains(templateName, "grpc") {
+		util.Logf("%v\n", code)
+	}
+
 	formatted, err := format.Source([]byte(code))
 
 	return string(formatted), err
--- protoc-gen-gokit-base/template_files/gokit-base-cmd/grpc_binding.go
+++ protoc-gen-gokit-base/template_files/gokit-base-cmd/grpc_binding.go
@@ -7,29 +7,41 @@ import (
 )
 
 type grpcBinding struct {
-	server.CurrencyExchangeService
+	server.{{.Service.GetName}}
 }
 
-func (b grpcBinding) ExchangeRateGetRate(ctx context.Context, in *pb.ExchangeRateGetRateRequest) (*pb.ExchangeRateGetRateResponse, error) {
+{{range $i := .Service.Methods}}
+func (b grpcBinding) {{$i.GetName}}(ctx context.Context, in *pb.{{$i.RequestType.GetName}}) (*pb.{{$i.ResponseType.GetName}}, error) {
 	ctx = context.WithValue(ctx, "transport", "grpc")
-	ctx = context.WithValue(ctx, "request-method", "ExchangeRateGetRate")
-	return b.CurrencyExchangeService.ExchangeRateGetRate(in)
+	ctx = context.WithValue(ctx, "request-method", "{{$i.RequestType.GetName}}")
+	return b.{{.Service.GetName}}.{{$i.GetName}}(in)
 }
+{{end}}
 
-func (b grpcBinding) ExchangeRateConvert(ctx context.Context, in *pb.ExchangeRateConvertRequest) (*pb.ExchangeRateConvertResponse, error) {
-	ctx = context.WithValue(ctx, "transport", "grpc")
-	ctx = context.WithValue(ctx, "request-method", "ExchangeRateConvert")
-	return b.CurrencyExchangeService.ExchangeRateConvert(in)
-}
 
-func (b grpcBinding) Status(ctx context.Context, in *pb.StatusRequest) (*pb.StatusResponse, error) {
-	ctx = context.WithValue(ctx, "transport", "grpc")
-	ctx = context.WithValue(ctx, "request-method", "Status")
-	return b.CurrencyExchangeService.Status(in)
-}
+// ORGINAL FOR COMPARISON
+// Note that on the context.WithValue for request-method that "Request" is stripped off the string, that may need to be done
 
-func (b grpcBinding) Ping(ctx context.Context, in *pb.PingRequest) (*pb.PingResponse, error) {
-	ctx = context.WithValue(ctx, "transport", "grpc")
-	ctx = context.WithValue(ctx, "request-method", "Ping")
-	return b.CurrencyExchangeService.Ping(in)
-}
+//func (b grpcBinding) ExchangeRateGetRate(ctx context.Context, in *pb.ExchangeRateGetRateRequest) (*pb.ExchangeRateGetRateResponse, error) {
+	//ctx = context.WithValue(ctx, "transport", "grpc")
+	//ctx = context.WithValue(ctx, "request-method", "ExchangeRateGetRate")
+	//return b.CurrencyExchangeService.ExchangeRateGetRate(in)
+//}
+
+//func (b grpcBinding) ExchangeRateConvert(ctx context.Context, in *pb.ExchangeRateConvertRequest) (*pb.ExchangeRateConvertResponse, error) {
+	//ctx = context.WithValue(ctx, "transport", "grpc")
+	//ctx = context.WithValue(ctx, "request-method", "ExchangeRateConvert")
+	//return b.CurrencyExchangeService.ExchangeRateConvert(in)
+//}
+
+//func (b grpcBinding) Status(ctx context.Context, in *pb.StatusRequest) (*pb.StatusResponse, error) {
+	//ctx = context.WithValue(ctx, "transport", "grpc")
+	//ctx = context.WithValue(ctx, "request-method", "Status")
+	//return b.CurrencyExchangeService.Status(in)
+//}
+
+//func (b grpcBinding) Ping(ctx context.Context, in *pb.PingRequest) (*pb.PingResponse, error) {
+	//ctx = context.WithValue(ctx, "transport", "grpc")
+	//ctx = context.WithValue(ctx, "request-method", "Ping")
+	//return b.CurrencyExchangeService.Ping(in)
+//}
