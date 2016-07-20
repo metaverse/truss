e0d0c3400ce5dd5a897d34b2e8bc06484c2951bb
Adam Ryman <adamr@tune.com>
2016-07-20 12:10:21 -0700

    Add sanitycheck.sh for installing and running truss-gokit just to make sure nothing is really broken

---
+++ sanitycheck.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -x #echo on
+
+# If any of these error we might have an issue 
+# Until we have a better development workflow then pushing to master
+go get github.com/TuneLab/gob/...
+cd protoc-gen-truss-gokit && \
+make && \
+make clean
