a5b00d119cf2708655b0003205fd2d1051a2f294
Adam Ryman <adamr@tune.com>
2016-08-01 15:26:03 -0700

    Refactor truss and integration_tests
    truss has been refactored into main.go and generator/generator.go.
    Where main.go deals with working directory, GOPATH, and argument
    parsing and sanitation and generator.go actually exec's protoc and go
    build to generate and build the microservice.
    
    integration_tests were refactored to use `testing.T` to do logging
    rather than logrus. As well as be more readable. It can also be called
    with `$ go test -tags=integration -clean` in order to remove the
    generated test files and nothing else.
    
    Eventual goals are to unit test generator.go's functions and
    eventually move the integration_tests to the generator package and test
    by calling functions rather than execing the truss binary

---
+++ truss/generator/generator.go
@@ -0,0 +1,186 @@
+package generator
+
+import (
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+
+	log "github.com/Sirupsen/logrus"
+
+	templates "github.com/TuneLab/gob/truss/template"
+)
+
+func init() {
+	log.SetLevel(log.DebugLevel)
+	log.SetFormatter(&log.TextFormatter{
+		ForceColors: true,
+	})
+}
+
+// GenerateMicroservice takes a golang importPath, a path to .proto definition files workingDirectory, and a slice of
+// definition files DefinitionFiles and outputs a ./service direcotry in workingDirectory with a generated and built golang microservice
+func GenerateMicroservice(importPath string, workingDirectory string, definitionFiles []string) {
+
+	done := make(chan bool)
+
+	// Stage 1
+	buildDirectories(workingDirectory)
+	outputGoogleImport(workingDirectory)
+
+	// Stage 2, 3, 4
+	generatePbGoCmd := "--go_out=Mgoogle/api/annotations.proto=" + importPath + "/service/DONOTEDIT/third_party/googleapis/google/api,plugins=grpc:./service/DONOTEDIT/pb"
+	const generateDocsCmd = "--truss-doc_out=."
+	const generateGoKitCmd = "--truss-gokit_out=."
+
+	go protoc(workingDirectory, definitionFiles, generatePbGoCmd, done)
+	go protoc(workingDirectory, definitionFiles, generateDocsCmd, done)
+	go protoc(workingDirectory, definitionFiles, generateGoKitCmd, done)
+
+	<-done
+	<-done
+	<-done
+
+	// Stage 5
+	go goBuild("server", importPath+"/service/DONOTEDIT/cmd/svc/...", done)
+	go goBuild("cliclient", importPath+"/service/DONOTEDIT/cmd/cliclient/...", done)
+
+	<-done
+	<-done
+}
+
+// buildDirectories puts the following directories in place
+// .
+// └── service
+//     ├── bin
+//     └── DONOTEDIT
+//         ├── pb
+//         └── third_party
+//             └── googleapis
+//                 └── google
+//                     └── api
+func buildDirectories(workingDirectory string) {
+	// third_party created by going through assets in template
+	// and creating directoires that are not there
+	for _, filePath := range templates.AssetNames() {
+		fullPath := workingDirectory + "/" + filePath
+
+		dirPath := filepath.Dir(fullPath)
+
+		err := os.MkdirAll(dirPath, 0777)
+		if err != nil {
+			log.WithField("DirPath", dirPath).WithError(err).Fatal("Cannot create directories")
+		}
+	}
+
+	// Create the directory where protoc will store the compiled .pb.go files
+	err := os.MkdirAll(workingDirectory+"/service/DONOTEDIT/pb", 0777)
+	if err != nil {
+		log.WithField("DirPath", "service/DONOTEDIT/pb").WithError(err).Fatal("Cannot create directories")
+	}
+
+	// Create the directory where go build will put the compiled binaries
+	err = os.MkdirAll(workingDirectory+"/service/bin", 0777)
+	if err != nil {
+		log.WithField("DirPath", "service/bin").WithError(err).Fatal("Cannot create directories")
+	}
+}
+
+// outputGoogleImport places imported and required google.api.http protobuf option files
+// into their required directories as part of stage one generation
+func outputGoogleImport(workingDirectory string) {
+	// Output files that are stored in template package
+	for _, filePath := range templates.AssetNames() {
+		fileBytes, _ := templates.Asset(filePath)
+		fullPath := workingDirectory + "/" + filePath
+
+		err := ioutil.WriteFile(fullPath, fileBytes, 0666)
+		if err != nil {
+			log.WithField("FilePath", fullPath).WithError(err).Fatal("Cannot create ")
+		}
+	}
+}
+
+// goBuild calls the `$ go get ` to install dependenices
+// and then calls `$ go build service/bin/$name $path`
+// to put the iterating binaries in the correct place
+func goBuild(name string, path string, done chan bool) {
+
+	// $ go get
+
+	goGetExec := exec.Command(
+		"go",
+		"get",
+		"-d",
+		"-v",
+		path,
+	)
+
+	goGetExec.Stderr = os.Stderr
+
+	log.WithField("cmd", strings.Join(goGetExec.Args, " ")).Info("go get")
+	val, err := goGetExec.Output()
+
+	if err != nil {
+		log.WithFields(log.Fields{
+			"output": string(val),
+			"input":  goGetExec.Args,
+		}).WithError(err).Warn("go get failed")
+	}
+
+	// $ go build
+
+	goBuildExec := exec.Command(
+		"go",
+		"build",
+		"-o",
+		"service/bin/"+name,
+		path,
+	)
+
+	goBuildExec.Stderr = os.Stderr
+
+	log.WithField("cmd", strings.Join(goBuildExec.Args, " ")).Info("go build")
+	val, err = goBuildExec.Output()
+
+	if err != nil {
+		log.WithFields(log.Fields{
+			"output": string(val),
+			"input":  goBuildExec.Args,
+		}).WithError(err).Fatal("go build failed")
+	}
+
+	done <- true
+}
+
+func protoc(workingDirectory string, definitionPaths []string, command string, done chan bool) {
+	const googleApiHttpImportPath = "/service/DONOTEDIT/third_party/googleapis"
+
+	cmdArgs := []string{
+		"-I.",
+		"-I" + workingDirectory + googleApiHttpImportPath,
+		command,
+	}
+	// Append each definition file path to the end of that command args
+	cmdArgs = append(cmdArgs, definitionPaths...)
+
+	protocExec := exec.Command(
+		"protoc",
+		cmdArgs...,
+	)
+
+	protocExec.Stderr = os.Stderr
+
+	log.WithField("cmd", strings.Join(protocExec.Args, " ")).Info("protoc")
+	val, err := protocExec.Output()
+
+	if err != nil {
+		log.WithFields(log.Fields{
+			"output": string(val),
+			"input":  protocExec.Args,
+		}).WithError(err).Fatal("Protoc call failed")
+	}
+
+	done <- true
+}
