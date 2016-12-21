ff2ad82bd2dfde89844e9e0fe714f691d7289194
Leland Batey <leland@tune.com>
2016-12-20 16:18:30 -0800

    Add hook for controlling shutdown in truss

---
+++ gengokit/handler/hooks.go
@@ -0,0 +1,31 @@
+package handler
+
+import (
+	"io"
+	"strings"
+
+	"github.com/TuneLab/go-truss/gengokit"
+)
+
+const HookPath = "NAME-service/handlers/server/hooks.gotemplate"
+
+// NewHook returns a new HookRender
+func NewHook(prev io.Reader) gengokit.Renderable {
+	return &HookRender{
+		prev: prev,
+	}
+}
+
+type HookRender struct {
+	prev io.Reader
+}
+
+// Render will return the existing file if it exists, otherwise it will return
+// a brand new copy from the template.
+func (h *HookRender) Render(_ string, _ *gengokit.Data) (io.Reader, error) {
+	if h.prev == nil {
+		return strings.NewReader(hookTempl), nil
+	} else {
+		return h.prev, nil
+	}
+}
--- gengokit/handler/templates.go
+++ gengokit/handler/templates.go
@@ -48,3 +48,23 @@ type {{.PackageName}}Service struct{}
 	{{end}}
 {{- end}}
 `
+const hookTempl = `
+package handler
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func InterruptHandler(errc chan<- error) {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+	terminateError := fmt.Errorf("%s", <-c)
+
+	// Place whatever shutdown handling you want here
+
+	errc <- terminateError
+}
+`
