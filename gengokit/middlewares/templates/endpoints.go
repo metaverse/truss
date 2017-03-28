01efd81a23285b5cd5eba400e70b3eb6ee695681
Adam Ryman <adamr@tune.com>
2017-03-28 12:17:20 -0700

    Add exclude functionality to endpoints.WrapAll

--- gengokit/middlewares/templates/endpoints.go
+++ gengokit/middlewares/templates/endpoints.go
@@ -4,6 +4,8 @@ const EndpointsBase = `
 package middlewares
 
 import (
+	_ "github.com/go-kit/kit/endpoint"
+
 	svc "{{.ImportPath -}} /generated"
 )
 
@@ -11,12 +13,16 @@ import (
 // set of middlewares can be wrapped around every middleware (e.g., access
 // logging and instrumentation), and others wrapped selectively around some
 // endpoints and not others (e.g., endpoints requiring authenticated access).
+// Note that the final middleware applied will be the outermost middleware
+// (i.e. applied first)
 func WrapEndpoints(in svc.Endpoints) svc.Endpoints {
 
 	// Pass in the middlewares you want applied to every endpoint.
-	in.WrapAll(/* ...endpoint.Middleware */)
+	// optionally pass in endpoints by name that you want to be excluded
+	// e.g.
+	// in.WrapAll(authMiddleware, "Status", "Ping")
 
-	// How to apply a middleware selectively.
+	// How to apply a middleware to a single endpoint.
 	// in.ExampleEndpoint = authMiddleware(in.ExampleEndpoint)
 
 	return in
