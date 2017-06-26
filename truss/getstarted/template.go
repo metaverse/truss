ad3ee8ce4a402c4f311879d90c7d3bb1d722c9cb
Leland Batey <leland@tune.com>
2017-06-26 15:37:06 -0700

    Add getstarted flag and protobuf file as output

---
+++ truss/getstarted/template.go
@@ -0,0 +1,48 @@
+package getstarted
+
+const starterProto = `
+syntax = "proto3";
+
+package {{.PackageName}};
+ 
+import "github.com/TuneLab/go-truss/deftree/googlethirdparty/annotations.proto";
+
+service {{.ServiceName}} {
+  rpc Status(StatusRequest) returns (StatusResponse) {
+    option (google.api.http) = {
+      get: "/status"
+    };
+  }
+}
+
+enum ServiceStatus {
+  FAIL = 0;
+  OK = 1;
+}
+
+message StatusRequest {
+  bool full = 1;
+}
+
+message StatusResponse {
+  ServiceStatus status = 1;
+}
+`
+
+const nextStepMsg = `A "starter" protobuf file named '{{.FileName}}' has been created in the
+current directory. You can generate a service based on this new protobuf file
+at any time using the following command:
+
+    truss {{.FileName}}
+
+If you want to generate a protofile with a different name, use the
+'--getstarted' option with the name of your choice after '--getstarted'. For
+example, to generate a 'foo.proto', use the following command:
+
+    truss --getstarted foo
+`
+const existingFileMsg = `There's already a "starter" protobuf file named '{{.FileName}}' in the current
+directory. If you'd like to generate a service based on this existing protobuf
+file, you should instead run the command:
+
+    truss {{.FileName}}`
