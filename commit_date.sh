ca1dd5549936a59f106d90f2dfafc525ab6a6ef3
Zaq? Wiedmann <zaquestion@gmail.com>
2020-09-05 19:14:08 -0700

    fix: use consistent datetime formate in version output
    also fixes goreleaser to use the commit date, which should be formatted as UTC rfc3999 per https://goreleaser.com/customization/templates/

--- commit_date.sh
+++ commit_date.sh
@@ -13,10 +13,10 @@ git_commit_epoc="$(git show -s --format=%ct $head_commit)"
 # bsd date does not have `--version`
 if [[ "$(date --version 2>/dev/null 1>/dev/null; echo $?)" -eq "1" ]]; then
 	# bsd date
-	commit_date=$(date -r $git_commit_epoc -u)
+	commit_date=$(date -r $git_commit_epoc -u +"%Y-%m-%dT%H:%M:%SZ")
 else
 	# gnu date
-	commit_date=$(date --date="@$git_commit_epoc" -u)
+	commit_date=$(date --date="@$git_commit_epoc" -u +"%Y-%m-%dT%H:%M:%SZ")
 fi
 
 echo $commit_date
